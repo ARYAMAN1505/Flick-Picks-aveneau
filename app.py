dc78015bc5e4897faa065160a466b3c7e1613a3e
Aryaman Somvanshi <aryaman1505@gmail.com>
2024-06-30 15:28:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import pickle
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -45,4 +46,5 @@ def show_recommendations(selected_movie):
                            recommended_posters=recommended_movie_posters)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
